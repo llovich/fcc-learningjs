f79569bb3e4a9bb66a0802cbbe55e9ff66c84ca6
girllogic <lillianlovich@yahoo.com>
2017-03-17 10:34:57 -0500

    added fcc's solution to comments

--- titleCaseSentence.js
+++ titleCaseSentence.js
@@ -5,6 +5,9 @@ Return the provided string with the first letter of each word capitalized. Make 
 
 For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
 
+after looking at fcc's solutions - could have used slice(1) instead of substring()
+from inside their for loop:  str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+
 */
 
 function titleCase(str) {
